a11439fd5fc129b9b43477cf4b35ddfdf798e57a
GuiLira <liraguilherme252@gmail.com>
2019-09-29 17:56:43 -0300

    Diminuição e implementação de função setInterval.

--- magica.js
+++ magica.js
@@ -1,35 +1,16 @@
 chrome.runtime.onInstalled.addListener(function() {    
-
-    chrome.tabs.getAllInWindow(null, callback);
-    let tabs_n = null;
-    function callback(data){
-        // console.log(data.length);
-        tabs_n = data.length;
-
-        chrome.storage.sync.set({abas: tabs_n});
-    }
+    setInterval(
+        function(){
+            chrome.tabs.getAllInWindow(null, callback);
+            let tabs_n = null;
+            function callback(data){
+                // console.log(data.length);
+                tabs_n = data.length;
     
-
-    function constructOptions() {
-        let h2 = document.createElement('h2');
-        let page = document.getElementById('page');
-        chrome.storage.sync.get('abas', function(result){
-            let abas_t = result.abas;
-            // console.log(abas_t); 
-            h2.innerHTML = abas_t;
-        });
-
-        // h2.addEventListener('click', function() {
-        //   chrome.storage.sync.set({color: item}, function() {
-        //     console.log('color is ' + item);
-        //   })
-        // });
-        page.appendChild(h2);
-    }
-      
-    constructOptions();
-
-    
+                chrome.storage.sync.set({abas: tabs_n});
+            }
+        }
+    , 1000);
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -40,24 +21,5 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
-
-    // let tag = document.getElementById('btn');
-    // console.log(tag);
-    
-    // let changeColor = document.getElementById('btn');
-    
-    // changeColor.onclick = function(element) {
-    //     let color = element.target.value;
-    //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //       chrome.tabs.executeScript(
-    //           tabs[0].id,
-    //           {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    //     });
-    // };
-
-    // chrome.storage.sync.get('color', function(data) {
-    //     changeColor.style.backgroundColor = data.color;
-    //     changeColor.setAttribute('value', data.color);
-    // });
 
 });
